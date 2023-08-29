6fc725cc0eb20a8891f75215e4c8ce5919834225
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2023-08-29 19:42:37 -0300

    atualização

--- bus2.py
+++ bus2.py
@@ -11,7 +11,14 @@ class Bus(Vehicle):
     # assign default value to capacity
     def seating_capacity(self, capacity=50):
         return super().seating_capacity(capacity=50)
+class moto(Vehicle):
+    # assign default value to capacity
+    def seating_capacity(self, capacity=2):
+        return super().seating_capacity(capacity=2)        
 
 School_bus = Bus("School Volvo", 180, 12)
+print(School_bus.seating_capacity())
+ 
+moto = moto("cd 500", 200, 32)
 print(School_bus.seating_capacity())
  
